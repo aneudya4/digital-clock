2e55865b9c128173b473890fd769ce6780060e6a
Aneudy Adames <a4aneudy@gmail.com>
2020-10-08 23:53:28 -0400

    added readme file

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ let is24Hours = false;
 const getTime = () => {
   const date = new Date();
   const timeOfTheDay = date.getHours() > 12 ? 'PM' : 'AM';
-  let hours = is24Hours ? date.getHours() : date.getHours() > 12 ? date.getHours() - 12: date.getHours();
+  let hours = is24Hours
+    ? date.getHours()
+    : date.getHours() > 12
+    ? date.getHours() - 12
+    : date.getHours();
   let minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
   let seconds = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
   timeContainer.textContent = `${hours}:${minutes}:${seconds} ${timeOfTheDay}`;
@@ -15,25 +19,23 @@ checkbox.addEventListener('change', (event) => {
   return (is24Hours = event.target.checked);
 });
 
-const enable24Hours = ()=>{
- const infoSpan = document.querySelector(".format-info")
- const date = new Date()
- const hour = date.getHours()
- if(hour <= 12){
- infoSpan.innerText = "24 hour format disabled before noon"
- checkbox.disabled = true
-}else{
-checkbox.disabled = false
-}
-
-
-}
+const enable24Hours = () => {
+  const infoSpan = document.querySelector('.format-info');
+  const date = new Date();
+  const hour = date.getHours();
+  console.log(hour);
+  if (hour <= 12) {
+    infoSpan.innerText = '24 hour format disabled before noon';
+    checkbox.disabled = true;
+  } else {
+    checkbox.disabled = false;
+  }
+};
 
 setInterval(getTime, 1000);
 
 window.addEventListener('load', (event) => {
   is24Hours = localStorage.getItem('is24Hours') === 'true' ? true : false;
   checkbox.checked = is24Hours;
-  enable24Hours()
-
+  enable24Hours();
 });
